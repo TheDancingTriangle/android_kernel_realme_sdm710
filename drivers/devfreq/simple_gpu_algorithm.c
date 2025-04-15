b02117e2b92a6a0c33641eba3a791c3a139d9f61
TheDancingTriangle <79113105+TheDancingTriangle@users.noreply.github.com>
2025-04-15 23:57:32 +0300

    sgatest2

--- drivers/devfreq/simple_gpu_algorithm.c
+++ drivers/devfreq/simple_gpu_algorithm.c
@@ -19,10 +19,10 @@
 #include <linux/devfreq.h>
 #include <linux/msm_adreno_devfreq.h>
 
-static int default_laziness = 3;
+static int default_laziness = 10;
 module_param_named(simple_laziness, default_laziness, int, 0664);
 
-static int ramp_up_threshold = 3000;
+static int ramp_up_threshold = 1000;
 module_param_named(simple_ramp_threshold, ramp_up_threshold, int, 0664);
 
 int simple_gpu_active = 1;
